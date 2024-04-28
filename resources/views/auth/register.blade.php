483a9a1d2136bd7714012c821679a8d857d96aa5
overdonebirch <alendez19@gmail.com>
2024-04-28 20:40:15 +0200

    añado bucle para mostrar periodos y precios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,15 +28,74 @@
             <input type="password" class="form-control w-100 texto-negro" step="any" id="formGroupExampleInput" name="password" min="0" placeholder="password">
         </div>
         <div class="mb-3">
-            <label for="categoria" class="form-label text-white">Eligen tu gimnasio:</label>
+            <label for="categoria" class="form-label text-white">Elige tu gimnasio:</label>
             <select class="form-select w-100" name="id_gimnasio">
                 @foreach ($gimnasios as $g)
                     <option value="{{ $g->_id }}" >{{ $g->nombre }}</option>
                  @endforeach
             </select>
         </div>
+        <div class="mb-3">
+            <label for="categoria" class="form-label text-white">Elige tu membresía: </label>
+            <select class="form-select w-100" name="id_gimnasio" id="membresia">
+                @foreach ($membresias as $m)
+                    <option value="{{ $m->_id }}">{{ $m->nombre }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mb-3" id="precios-div" style="display: none;">
+            <label for="precio" class="form-label text-white">Elige el precio y período : </label>
+            <select class="form-select w-100" name="precio" id="precios">
+
+            </select>
+        </div>
+
     <button type="submit" class="btn btn-light">Registrarse</button>
 </div>
 </form>
+
+
+{{-- Cambiar los precios mostrados:  --}}
+<script>
+
+    const selectMembresia = document.getElementById('membresia');
+
+    const preciosDiv = document.getElementById('precios-div');
+
+    const selectPrecios = document.getElementById('precios');
+
+
+    selectMembresia.addEventListener('change', function() {
+
+        const selectedMembresiaId = selectMembresia.value;
+
+
+        if (selectedMembresiaId) {
+
+            preciosDiv.style.display = 'block';
+
+
+            const selectedMembresia = {!! json_encode($membresias) !!}.find(m => m._id === selectedMembresiaId);
+
+            selectPrecios.innerHTML = '';
+
+            // Seleccionar el periodo y precios :
+            selectedMembresia.periodos_meses.forEach(meses => {
+
+                let valorTotal = selectedMembresia.precio * meses;
+                let option = document.createElement('option');
+                option.textContent = meses+ " meses : " +valorTotal + " €";
+                option.value = valorTotal+"|"+meses ;
+                selectPrecios.appendChild(option);
+
+            });
+
+
+        } else {
+            preciosDiv.style.display = 'none';
+        }
+    });
+</script>
 
 @endsection
