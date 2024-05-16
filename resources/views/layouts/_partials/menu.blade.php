347b95060ba0eff3751022e360cee89eb2755da4
overdonebirch <alendez19@gmail.com>
2024-05-16 15:37:36 +0200

    añado enlace al calendario de clases

--- resources/views/layouts/_partials/menu.blade.php
+++ resources/views/layouts/_partials/menu.blade.php
@@ -2,7 +2,7 @@
     <section class="row  cabecera d-flex align-items-center">
         <div class="col-md-3 d-flex justify-content-center"><a href="{{route('inicio')}}" class="titulo">Vitality</a></div>
         <div class="col-md-3 d-flex justify-content-center"><a href="{{route('dondeEstamos')}}">Donde estamos</a></div>
-        <div class="col-md-1 d-flex justify-content-center"><a>Ver Clases</a></div>
+        <div class="col-md-1 d-flex justify-content-center"><a href="{{route('clases')}}">Ver Clases</a><</div>
         <div class="col-md-2 d-flex justify-content-md-end justify-content-center mt-md-0 mt-3">
             <a href="{{route ('formRegistro')}}" type="button" class="btn boton justify-content-center textoBoton color-principal">Hazte Socio</a>
         </div>
