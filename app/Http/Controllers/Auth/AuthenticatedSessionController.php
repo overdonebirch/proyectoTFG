521ea8db96a96aa6583bc72655b1558aeb04c9b9
overdonebirch <alendez19@gmail.com>
2024-05-19 15:42:29 +0200

    cambio el return para redirigir al inicio una vez logado

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect('/inicio')->with("success","Logado Correctamente");
     }
 
     /**
