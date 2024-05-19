67cb93c3008de159d33cf80ffdd78cd9bb51aa69
overdonebirch <alendez19@gmail.com>
2024-05-19 15:45:48 +0200

    muestro el boton de login y logout

--- resources/views/layouts/_partials/menu.blade.php
+++ resources/views/layouts/_partials/menu.blade.php
@@ -6,8 +6,26 @@
         <div class="col-md-2 d-flex justify-content-md-end justify-content-center mt-md-0 mt-3">
             <a href="{{route ('formRegistro')}}" type="button" class="btn boton justify-content-center textoBoton color-principal">Hazte Socio</a>
         </div>
-        <div class="col-md-2 d-flex justify-content-md-start justify-content-center mt-md-0 mt-3">
-            <a href="" type="button" class="btn boton justify-content-center textoBoton color-secundario">Inicia Ssesión</a>
-        </div>
+
+        @if (Route::has('login'))
+
+            @auth
+
+                <div class="col-md-2 d-flex justify-content-md-start justify-content-center mt-md-0 mt-3">
+                    <a href="{{route ('logout')}}"type="button" class="btn boton justify-content-center textoBoton color-secundario">Cerrar Sesión</a>
+                </div>
+
+            @else
+
+                <div class="col-md-2 d-flex justify-content-md-start justify-content-center mt-md-0 mt-3">
+                    <a href="{{route ('login')}}"type="button" class="btn boton justify-content-center textoBoton color-secundario">Iniciar Sesión</a>
+                </div>
+
+            @endauth
+
+        @endif
+
+
+
     </section>
 </header>
