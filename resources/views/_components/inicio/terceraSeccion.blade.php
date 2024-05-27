33e06e8d9c4cba0ae44f324781c8c5e9588f2da9
overdonebirch <alendez19@gmail.com>
2024-05-27 22:21:52 +0200

    cambio la vista del inicio

--- resources/views/_components/inicio/terceraSeccion.blade.php
+++ resources/views/_components/inicio/terceraSeccion.blade.php
@@ -1,12 +1,12 @@
 
 <section class="row inicio__terceraSeccion imagenFondo container-fluid margin-top-big">
     <div class="col-md-12 d-flex justify-content-center">
-        <p class="mt-5 textoPromocional__subtitulo ">Disfruta de las mejores clases para tonificar</p>
+        <p class="mt-5 textoPromocional__subtitulo ">No estas registrado y quieres ver tus reservas de clases?</p>
     </div>
     <div class="col-md-12 d-flex justify-content-start margen-pag-inicio align-items-end">
-        <p class="mt-5 textoPromocional__subtitulo">*Acceso ilimitado con membresía Premium</p>
+        <p class="mt-5 textoPromocional__subtitulo"></p>
     </div>
     <div class="col-md-12 d-flex justify-content-start margen-pag-inicio">
-        <button type="button" class="btn boton-ancho justify-content-center textoBoton color-principal">Hazte Premium</button>
+        <a type="button" href="{{route('dniReserva')}}" class="btn boton-ancho justify-content-center textoBoton color-principal">Ver tus clases</a>
     </div>
 </section>
